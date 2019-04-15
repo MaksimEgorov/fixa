814b50e03fc23561764a25bd4867ed9615ef8c31
MaksimEgorov <egorov.gps@gmail.com>
2019-04-15 14:50:45 +0100

    mobile menu

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,3 +7,37 @@ console.log('\'Allo \'Allo!');
 // Uncomment to enable Bootstrap popovers
 // https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
 // $(function () { $('[data-toggle="popover"]').popover(); });
+
+$(function () {
+
+    var $html = $('html');
+    var $body = $('body');
+  
+    /* Photos changing on hover */
+    /* $('.js-production-menu').each(function () {
+      var $list = $(this);
+      var $images = $list.find('.js-production-menu-images');
+      $list.find('[data-image-ref]').on('mouseenter', function () {
+        var imageRef = $(this).data('image-ref');
+        $images.find(`[data-image-item="${imageRef}"]`).addClass('active').siblings('.active').removeClass(
+          'active');
+      });
+    }); */
+  
+  
+    /* Scroll to element on click */
+    $('[data-scroll-to]').on('click', function (e) {
+      e.preventDefault();
+      scrollToElement('#' + $(this).data('scroll-to'));
+    });
+  
+    $('.js-navigation').each(function () {
+      var $navigation = $(this);
+      var $button = $navigation.find('.js-mobile-menu-button');
+      console.log($button)
+      $button.on('click', function(e) {
+        e.preventDefault();
+        $html.toggleClass('html-navigation-mobile-open');
+      });
+    });
+  });
